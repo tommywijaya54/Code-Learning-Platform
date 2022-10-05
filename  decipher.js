7b6c65bd7f319a7bb7d6072c39df650e4d1b4811
tommywijaya54 <95896676+tommywijaya54@users.noreply.github.com>
2022-10-06 03:00:43 +1100

    4 files modified

---  decipher.js
+++  decipher.js
@@ -1,3 +1,58 @@
-export function hello() {
-  return 'Hello';
+class CodeDechipher {
+  constructor(code) {
+    this.code = code;
+    this.value_from_find;
+    this.leftover = code;
+  }
+  find(_start, _end) {
+    var startindex = this.code.indexOf(_start);
+    var endindex = this.code.indexOf(_end, startindex);
+    const extracted_line =
+      startindex != -1 && endindex != -1 && endindex > startindex
+        ? this.code.substring(startindex, endindex)
+        : '';
+    this.leftover = this.leftover.replace(extracted_line, '');
+    echo = this.leftover.replace(/^\s+|\s+$/g, '');
+    return extracted_line;
+  }
+  findvalue(s, e) {
+    return this.find(s, e)
+      .replace(s + '=', '')
+      .trim();
+  }
+  _if(_startTag) {
+    this.cmd = _startTag;
+    if (_startTag == 'leftover') {
+      this.value = this.leftover;
+      return this;
+    }
+
+    this.value = this.findvalue(_startTag, '\n');
+    return this;
+  }
+  _do(cmd) {
+    if (this.cmd === 'leftover') {
+      write(cmd, this.value, 'pre');
+    } else if (this.value.includes(';')) {
+      this.value
+        .split(';')
+        .filter((element) => element)
+        .forEach((v) => write(cmd, v));
+    } else if (typeof this.value == 'string') {
+      write(cmd, this.value);
+    }
+  }
+  // process() {}
+  translate() {}
+  transform() {}
 }
+
+const dcper = (code) => {
+  let d = new CodeDechipher(code);
+  d._if('-t')._do('create tag');
+  d._if('-?')._do('create title');
+  d._if('leftover')._do('create code panel');
+  d.translate();
+};
+
+exports.dcper = dcper;
--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 // Import stylesheets
 
+import { dcper } from './decipher.js';
+
 import './style.css';
 const appDiv = document.getElementById('app');
 const code = document.getElementById('code-content');
@@ -7,49 +9,41 @@ const action = document.getElementById('action-todo');
 const echoDiv = document.getElementById('echo');
 var echo = 'no echo atm';
 
-const write = (dostr, valuestr) => {
-  
+const write = (dostr, valuestr, pre) => {
   const todo = document.createElement('div');
   todo.className = 'action';
-  todo.innerHTML = `
+
+  if (pre == 'pre') {
+    todo.innerHTML = `
+    <span class="action-cmd">${dostr}</span>
+    <span class='action-value'><pre>${valuestr}</pre></span>`;
+  } else {
+    todo.innerHTML = `
     <span class="action-cmd">${dostr}</span>
     <span class='action-value'>${valuestr}</span>`;
-
+  }
   action.appendChild(todo);
 };
-
-write('create tag', 'laravel');
-
-class CodeDechipher {
-  constructor(code) {
-    this.code = code;
-  }
-  find(_start, _end) {
-    var startindex = this.code.indexOf(_start);
-    var endindex = this.code.indexOf(_end, startindex);
-    const extracted =
-      startindex != -1 && endindex != -1 && endindex > startindex
-        ? this.code.substring(startindex, endindex)
-        : '';
-    echo = extracted;
-  }
-  _if(_startTag) {
-    this.find(_startTag, '\n');
-    return this;
-  }
-  _do(cmd, val) {
-    write(cmd, val);
+/*
+class Util {
+  static _loop(val, fn) {
+    if (typeof val == 'string') {
+      fn(cmd, val);
+    } else if (Array.isArray(val)) {
+      val.forEach((v) => fn(cmd, v));
+    }
   }
 }
-
-const dcper = (code) => {
-  let d = new CodeDechipher(code);
-  //d.find('-t', '\n');
-  //d.find('-?', '\n');
-  d._if('-t')._do('create tag', 'xxx');
-};
+*/
 
 dcper(code.innerHTML);
+
+code.addEventListener('blur', function () {
+  action.innerHTML = '';
+  dcper(code.innerHTML);
+});
+
+// Ending Code
 if (typeof echo == 'string') {
   echoDiv.innerText = echo;
 } else {
